47f583ac3a79d54261d2a350bc11618ebc64b0d7
Juan Carlos Peña Peña <penapena@uninorte.edu.co>
2020-10-19 18:22:51 -0500

    Users component Implemented

---
+++ src/app/users/user.service.ts
@@ -0,0 +1,23 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { User } from './user';
+import { Response } from './../shared/response'
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UsersService {
+
+  constructor(private http: HttpClient) { }
+
+  get(token: string): Observable<Response<User[]>> {
+    const url = `http://localhost:8080/user/`;
+    const httpOptions = {
+      headers: new HttpHeaders({
+        'Authorization': token
+      })
+    };
+    return this.http.get<Response<User[]>>(url, httpOptions);
+  }
+}
