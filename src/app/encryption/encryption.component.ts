66a6ac42382c3a22f0beb34e4d7c1a4b2d526cad
Juan Carlos Peña Peña <ing.juanca@gmail.com>
2020-10-21 17:38:28 -0500

    Downloading file first version

--- src/app/encryption/encryption.component.ts
+++ src/app/encryption/encryption.component.ts
@@ -44,4 +44,28 @@ export class EncryptionComponent implements OnInit {
     }
   }
 
+  downloadFile() {
+    console.log('Downloading file');
+    /*this.cargueService.obtenerArchivoSeleccionado(idFile).subscribe(respuesta => {
+      this.generarArchivoPlano(nombreArchivo, (respuesta.archivo));
+    });*/
+  }
+
+  private generarArchivoPlano(nombreArchivo: string, texto: string) {
+    const setting = {
+      element: {
+        dynamicDownload: null as HTMLElement
+      }
+    };
+    if (!setting.element.dynamicDownload) {
+      setting.element.dynamicDownload = document.createElement('a');
+    }
+    const element = setting.element.dynamicDownload;
+    const fileType = 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet';
+    element.setAttribute('href', `data:${fileType};base64,${(texto)}`);
+    element.setAttribute('download', nombreArchivo);
+    const event = new MouseEvent('click');
+    element.dispatchEvent(event);
+  }
+
 }
