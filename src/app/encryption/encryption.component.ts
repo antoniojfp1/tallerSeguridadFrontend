9f8161cca04fbc519e443703f9cd44ad130cd474
Juan Carlos Peña Peña <ing.juanca@gmail.com>
2020-10-21 14:28:04 -0500

    Upload File to Encryption

--- src/app/encryption/encryption.component.ts
+++ src/app/encryption/encryption.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-encription',
@@ -7,9 +8,40 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EncryptionComponent implements OnInit {
 
-  constructor() { }
+  nombreArchivo: string;
+  uploadForm: FormGroup;
+
+  constructor(private formBuilder: FormBuilder) { }
 
   ngOnInit(): void {
+    this.uploadForm = this.formBuilder.group({
+      profile: ['']
+    });
+  }
+
+  seleccionarArchivo(event: any) {
+    event.preventDefault();
+    const fileHiden = document.getElementById('fileHiden');
+    fileHiden.click();
+  }
+
+  encryptFile(){
+    const formData = new FormData();
+    formData.append('file', this.uploadForm.get('profile').value);
+    console.log('Enviado');
+  }
+
+  onFileChange(event: any) {
+    console.log(event);
+    const reader = new FileReader();
+    if (event.target && event.target.files.length > 0) {
+      const file = event.target.files[0];
+      this.uploadForm.get('profile').setValue(file);
+        reader.readAsDataURL(file);
+        reader.onload = (() => {
+          this.nombreArchivo = file.name;
+        });
+    }
   }
 
 }
