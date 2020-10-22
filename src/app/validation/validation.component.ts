f58625b8a744416f0690bd3ae57514857611de97
Juan Carlos Peña Peña <penapena@uninorte.edu.co>
2020-10-22 16:49:31 -0500

    RSA Implementation initial implementation

--- src/app/validation/validation.component.ts
+++ src/app/validation/validation.component.ts
@@ -13,12 +13,18 @@ export class ValidationComponent implements OnInit {
   uploadForm: FormGroup;
   publicKey: string;
   privateKey: string;
+  publicKeyNameFile: string;
+  fileUploaded: string;
 
   constructor(private fileService: FileService,
               private storageService: StorageService,
               private formBuilder: FormBuilder) { }
 
   ngOnInit(): void {
+    this.uploadForm = this.formBuilder.group({
+      publicKeyFile: [''],
+      file: ['']
+    });
   }
 
   generateKeys() {
@@ -27,6 +33,80 @@ export class ValidationComponent implements OnInit {
       this.publicKey = response.data.publicKey.content;
       this.privateKey = response.data.privateKey.content;
     });
+  }
+
+  downloadPublicKey() {
+    const setting = {
+      element: {
+        dynamicDownload: null as HTMLElement
+      }
+    };
+    if (!setting.element.dynamicDownload) {
+      setting.element.dynamicDownload = document.createElement('a');
+    }
+    const element = setting.element.dynamicDownload;
+    const fileType = 'application/msword';
+    element.setAttribute('href', `data:${fileType};base64,${(this.publicKey)}`);
+    element.setAttribute('download', 'application.bin');
+    const event = new MouseEvent('click');
+    element.dispatchEvent(event);
+  }
+
+  downloadPrivateKey() {
+    const setting = {
+      element: {
+        dynamicDownload: null as HTMLElement
+      }
+    };
+    if (!setting.element.dynamicDownload) {
+      setting.element.dynamicDownload = document.createElement('a');
+    }
+    const element = setting.element.dynamicDownload;
+    const fileType = 'application/msword';
+    element.setAttribute('href', `data:${fileType};base64,${(this.privateKey)}`);
+    element.setAttribute('download', 'application.bin');
+    const event = new MouseEvent('click');
+    element.dispatchEvent(event);
+  }
+
+  encryptFile() {
+
+  }
+
+  selectPublicKey(event: any) {
+    event.preventDefault();
+    const fileHiden = document.getElementById('publicKeyFileHiden');
+    fileHiden.click();
+  }
+
+  selectFile(event: any) {
+    event.preventDefault();
+    const fileHiden = document.getElementById('fileHiden');
+    fileHiden.click();
+  }
+
+  onPublicKeyFileChange(event: any) {
+    const reader = new FileReader();
+    if (event.target && event.target.files.length > 0) {
+      const file = event.target.files[0];
+      this.uploadForm.get('publicKeyFile').setValue(file);
+        reader.readAsDataURL(file);
+        reader.onload = (() => {
+          this.publicKeyNameFile = file.name;
+        });
+    }
+  }
+
+  onFileChange(event: any) {
+    const reader = new FileReader();
+    if (event.target && event.target.files.length > 0) {
+      const file = event.target.files[0];
+      this.uploadForm.get('file').setValue(file);
+        reader.readAsDataURL(file);
+        reader.onload = (() => {
+          this.fileUploaded = file.name;
+        });
+    }
   }
 
 }
